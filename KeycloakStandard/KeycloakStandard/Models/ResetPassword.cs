feaee1f065d9e5ce91ac165becf34b638b200b4b
Antonio Halužan <ryukote@outlook.com>
2024-06-25 16:08:59 +0200

    Password reset

--- KeycloakStandard/KeycloakStandard/Models/ResetPassword.cs
+++ KeycloakStandard/KeycloakStandard/Models/ResetPassword.cs
@@ -1,9 +1,14 @@
-﻿namespace KeycloakStandard.Models
+﻿using Newtonsoft.Json;
+
+namespace KeycloakStandard.Models
 {
     public class ResetPassword
     {
+        [JsonProperty("type")]
         public string Type { get; set; }
+        [JsonProperty("temporary")]
         public bool Temporary { get; set; }
+        [JsonProperty("value")]
         public string Value { get; set; }
     }
 }
